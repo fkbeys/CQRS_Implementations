eb9bb04d43c8df6470bcb322f752b3e41290dc2c
fkbeys <fkbeys@gmail.com>
2023-05-25 18:23:28 +0200

    docker file added

--- src/Presentation/Sales.WebApi/Program.cs
+++ src/Presentation/Sales.WebApi/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options => { options.AddDefaultPolicy(builder => { buil
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
